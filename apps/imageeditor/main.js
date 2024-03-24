2bff47f7661ce4242fc6c684de957d3b6cd4c3cd
lightbulbmeow <62456216+lightbulbmeow@users.noreply.github.com>
2024-03-24 20:27:29 +0800

    updates to Javascript image editor

--- apps/imageeditor/main.js
+++ apps/imageeditor/main.js
@@ -6,8 +6,8 @@ const ctx = canvas.getContext('2d')
 
 const srcImage = new Image
 
-let imgData, pix
-let w,x,y,r,g,b,a
+let imgData
+let w,h,r,g,b,a
 
 fileinput.onchange = function (e) {
   if (e.target.files && e.target.files.item(0)) {
@@ -16,45 +16,63 @@ fileinput.onchange = function (e) {
 }
 
 function resetImage(){
-  canvas.width = srcImage.width
-  canvas.height = srcImage.height
-  w = canvas.width
-  h = canvas.height
-  ctx.drawImage(srcImage, 0, 0, srcImage.width, srcImage.height)
-  imgData = ctx.getImageData(0, 0, srcImage.width, srcImage.height)
+  w = srcImage.width
+  h = srcImage.height
+  canvas.width = w
+  canvas.height = h
+  ctx.drawImage(srcImage, 0, 0, w, h)
+  imgData = ctx.getImageData(0, 0, w, h) // the data is given as an array of 4*w*h bytes [r,g,b,a,r,g,b,a,...]
 }
 
+srcImage.src = "manunchan.png"
+srcImage.setAttribute('crossOrigin', '') // cors nonsense
 srcImage.onload = resetImage
 
-function getcoord(x,y){
-  return (x + y * w) * 4
-}
-
-function pixel(x,y){
-  coord = getcoord(x,y)
-  return [imgData.data[coord], imgData.data[coord+1], imgData.data[coord+2], imgData.data[coord+3]]
-}
 
 // code handling
 
+const loading = document.getElementById('loading')
 const inputcode = document.getElementById('inputcode')
 
+function pixel(x,y){
+  x = Math.round(x)
+  y = Math.round(y)
+  if(x < 0 || x >= w || y < 0 || y >= h) return [0,0,0,0] // pixel is out of bounds
+  coord = (x + y * canvas.width) * 4
+  return [imgData.data[coord], imgData.data[coord+1], imgData.data[coord+2], imgData.data[coord+3]]
+}
+
 function applyCode(){
+  loading.innerText = "Loading..."
+  setTimeout(runCode)
+}
+
+function runCode(){
   eval(inputcode.value);
-  //newdata = new Uint8ClampedArray(imgData.data.length)
+  w = Math.trunc(w)
+  h = Math.trunc(h)
+  newimgData = new ImageData(w,h)
 
   for(let x = 0; x < w; x ++){
     for(let y = 0; y < h; y ++){
       [r,g,b,a] = pixel(x,y)
       rgba = setpixel(x,y)
-      if(rgba.length == 3) rgba.push(255)
-      coord = getcoord(x,y)
-      arr = [0,1,2,3]
-      arr.forEach(i => {imgData.data[coord+i] = rgba[i]})
+      if(rgba.length == 3) rgba.push(a)
+      coord = (x + y * w) * 4
+      for(const i of [0,1,2,3]) newimgData.data[coord+i] = rgba[i]
     }
   }
+
+  imgData = newimgData
+  canvas.width = w
+  canvas.height = h
   ctx.putImageData(imgData, 0, 0, 0, 0, w, h)
+
+  loading.innerText = "Done!"
 }
+
+
+// image saving
 
 function saveImage(){
     const createEl = document.createElement('a');
---
+++ apps/imageeditor/preset.js
@@ -0,0 +1,55 @@
+// file handling
+
+const examplecodes = document.getElementById('examplecodes')
+
+const codelist = {
+
+'Flip horizontal': `function setpixel(x,y){
+    return pixel(w-1-x,y)
+}`,
+
+'Rotate 90°': `[w,h] = [h,w]
+function setpixel(x,y){
+    return pixel(y,w-1-x)
+}`,
+
+'Shrink': `w /= 2
+h /= 2
+function setpixel(x,y){
+    return pixel(2*x,2*y)
+}`,
+
+'Invert colors': `function setpixel(x,y){
+    return [255-r,255-g,255-b]
+}`,
+
+'Hue shift': `function setpixel(x,y){
+    return [g,b,r]
+}`,
+
+'Edge detection': `function setpixel(x,y){
+    return [0,1,2].map(i => Math.abs(pixel(x,y)[i] - pixel(x-1,y)[i]))
+}`,
+
+'Light mode': `function setpixel(x,y){
+    y = 0.2126*r + 0.7152*g + 0.0722*b
+    return [255,255,255,y]
+}`,
+
+'Swirl': `function setpixel(x,y){
+    x -= w/2
+    y -= h/2
+    dist = Math.sqrt(x*x + y*y)
+    angle = Math.atan2(y,x) + dist/100
+    return pixel(w/2 + dist * Math.cos(angle), h/2 + dist * Math.sin(angle))
+}`,
+
+}
+
+for(const [key, value] of Object.entries(codelist)){
+  examplecodes.innerHTML += '<button onclick="presetCode(`' + value + '`)">' + key + '</button> '
+}
+
+function presetCode(value){
+  inputcode.value = value
+}
