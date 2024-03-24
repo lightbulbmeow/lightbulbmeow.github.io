c91b71e8f4f81c2f470bd0c180e7d02db272cc4d
lightbulbmeow <62456216+lightbulbmeow@users.noreply.github.com>
2024-03-24 18:02:49 +0800

    updates to image editor

--- apps/imageeditor/main.js
+++ apps/imageeditor/main.js
@@ -1,11 +1,13 @@
+// file handling
+
 const fileinput = document.getElementById('fileinput')
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
 const srcImage = new Image
 
-let imgData = null
-let originalPixels = null
+let imgData, pix
+let w,x,y,r,g,b,a
 
 fileinput.onchange = function (e) {
   if (e.target.files && e.target.files.item(0)) {
@@ -13,136 +15,45 @@ fileinput.onchange = function (e) {
   }
 }
 
-srcImage.onload = function () {
+function resetImage(){
   canvas.width = srcImage.width
   canvas.height = srcImage.height
+  w = canvas.width
+  h = canvas.height
   ctx.drawImage(srcImage, 0, 0, srcImage.width, srcImage.height)
   imgData = ctx.getImageData(0, 0, srcImage.width, srcImage.height)
-  originalPixels = imgData.data.slice()
 }
 
-function getIndex(x, y) {
-  return (x + y * srcImage.width) * 4
+srcImage.onload = resetImage
+
+function getcoord(x,y){
+  return (x + y * w) * 4
 }
 
-const red = document.getElementById('red')
-const green = document.getElementById('green')
-const blue = document.getElementById('blue')
-const brightness = document.getElementById('brightness')
-const grayscale = document.getElementById('grayscale')
-const contrast = document.getElementById('contrast')
-
-red.onchange = runPipeline
-green.onchange = runPipeline
-blue.onchange = runPipeline
-brightness.onchange = runPipeline
-grayscale.onchange = runPipeline
-contrast.onchange = runPipeline
-
-function clamp(value) {
-  return Math.max(0, Math.min(Math.floor(value), 255))
+function pixel(x,y){
+  coord = getcoord(x,y)
+  return [imgData.data[coord], imgData.data[coord+1], imgData.data[coord+2], imgData.data[coord+3]]
 }
 
-const R_OFFSET = 0
-const G_OFFSET = 1
-const B_OFFSET = 2
+// code handling
 
-function addRed(x, y, value) {
-  const index = getIndex(x, y) + R_OFFSET
-  const currentValue = currentPixels[index]
-  currentPixels[index] = clamp(currentValue + value)
-}
+const inputcode = document.getElementById('inputcode')
 
-function addGreen(x, y, value) {
-  const index = getIndex(x, y) + G_OFFSET
-  const currentValue = currentPixels[index]
-  currentPixels[index] = clamp(currentValue + value)
-}
+function applyCode(){
+  eval(inputcode.value);
+  //newdata = new Uint8ClampedArray(imgData.data.length)
 
-function addBlue(x, y, value) {
-  const index = getIndex(x, y) + B_OFFSET
-  const currentValue = currentPixels[index]
-  currentPixels[index] = clamp(currentValue + value)
-}
-
-function addBrightness(x, y, value) {
-  addRed(x, y, value)
-  addGreen(x, y, value)
-  addBlue(x, y, value)
-}
-
-function addContrast(x, y, value) {
-  const redIndex = getIndex(x, y) + R_OFFSET
-  const greenIndex = getIndex(x, y) + G_OFFSET
-  const blueIndex = getIndex(x, y) + B_OFFSET
-
-  const redValue = currentPixels[redIndex]
-  const greenValue = currentPixels[greenIndex]
-  const blueValue = currentPixels[blueIndex]
-
-  const alpha = (value + 255) / 255 // Goes from 0 to 2, where 0 to 1 is less contrast and 1 to 2 is more contrast
-
-  const nextRed = alpha * (redValue - 128) + 128
-  const nextGreen = alpha * (greenValue - 128) + 128
-  const nextBlue = alpha * (blueValue - 128) + 128
-
-  currentPixels[redIndex] = clamp(nextRed)
-  currentPixels[greenIndex] = clamp(nextGreen)
-  currentPixels[blueIndex] = clamp(nextBlue)
-}
-
-function setGrayscale(x, y) {
-  const redIndex = getIndex(x, y) + R_OFFSET
-  const greenIndex = getIndex(x, y) + G_OFFSET
-  const blueIndex = getIndex(x, y) + B_OFFSET
-
-  const redValue = currentPixels[redIndex]
-  const greenValue = currentPixels[greenIndex]
-  const blueValue = currentPixels[blueIndex]
-
-  const mean = (redValue + greenValue + blueValue) / 3
-
-  currentPixels[redIndex] = clamp(mean)
-  currentPixels[greenIndex] = clamp(mean)
-  currentPixels[blueIndex] = clamp(mean)
-}
-
-function runPipeline() {
-  currentPixels = originalPixels.slice()
-
-  const grayscaleFilter = grayscale.checked
-  const brightnessFilter = Number(brightness.value)
-  const contrastFilter = Number(contrast.value)
-  const redFilter = Number(red.value)
-  const greenFilter = Number(green.value)
-  const blueFilter = Number(blue.value)
-
-  for (let i = 0; i < srcImage.height; i++) {
-    for (let j = 0; j < srcImage.width; j++) {
-      if (grayscaleFilter) {
-        setGrayscale(j, i)
-      }
-
-      addBrightness(j, i, brightnessFilter)
-      addContrast(j, i, contrastFilter)
-
-      if (!grayscaleFilter) {
-        addRed(j, i, redFilter)
-        addGreen(j, i, greenFilter)
-        addBlue(j, i, blueFilter)
-      }
+  for(let x = 0; x < w; x ++){
+    for(let y = 0; y < h; y ++){
+      [r,g,b,a] = pixel(x,y)
+      rgba = setpixel(x,y)
+      if(rgba.length == 3) rgba.push(255)
+      coord = getcoord(x,y)
+      arr = [0,1,2,3]
+      arr.forEach(i => {imgData.data[coord+i] = rgba[i]})
     }
   }
-
-  commitChanges()
-}
-
-function commitChanges() {
-  for (let i = 0; i < imgData.data.length; i++) {
-    imgData.data[i] = currentPixels[i]
-  }
-
-  ctx.putImageData(imgData, 0, 0, 0, 0, srcImage.width, srcImage.height)
+  ctx.putImageData(imgData, 0, 0, 0, 0, w, h)
 }
 
 function saveImage(){
