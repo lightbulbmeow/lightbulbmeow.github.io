07cc676f47d082d5526c1c7dc0625aea41a5f8e2
lightbulbmeow <62456216+lightbulbmeow@users.noreply.github.com>
2024-08-18 10:11:41 +0800

    upload a few more stuff

--- apps/cipher/braille.js
+++ apps/cipher/braille.js
@@ -4,19 +4,56 @@ const inputcode = document.getElementById('inputcode')
 const output = document.getElementById('output')
 const malformed = document.getElementById('malformed')
 
+const allowdigits = document.getElementById('allowdigits')
+const allowspaces = document.getElementById('allowspaces')
+const allowsymbols = document.getElementById('allowsymbols')
+const nutrimatic = document.getElementById('nutrimatic')
+
 // actual code
 
-numberletters = [32,48,36,38,34,52,54,50,20,22,40,56,44,46,42,60,62,58,28,30,41,57,23,45,47,43]
-brailletoletters = {0:' ',15:'#',19:'.',16:',',24:';',18:':',12:'/',25:'?',26:'!',10:'#',11:'"',8:"'",49:'<',14:'>',27:'(',1:'^',9:'_',61:'&'}
-brailletodigits = {0:' ',3:'#',22:'0',19:'.',16:',',24:';',18:'-',12:'/',25:'?',26:'+',10:'#',11:'"',8:"'",49:'<',14:'>',27:'(',1:'^',9:'_',61:'&'}
+let brailletoletters
+let brailletodigits
 
-for(let i = 0; i < 26; i ++) brailletoletters[numberletters[i]] = String.fromCharCode(97+i);
-for(let i = 0; i < 9; i ++) brailletodigits[numberletters[i]] = String.fromCharCode(49+i);
+function inittable(){
+  numberletters = [32,48,36,38,34,52,54,50,20,22,40,56,44,46,42,60,62,58,28,30,41,57,23,45,47,43]
 
-function getchar(c){
+  brailletoletters = {}
+  brailletodigits = {}
+  for(let i = 0; i < 26; i ++) brailletoletters[numberletters[i]] = String.fromCharCode(65+i);
+
+  if(allowdigits.checked){
+    brailletoletters[15] = '#'
+    brailletodigits[3] = '#'
+    brailletodigits[22] = '0'
+    for(let i = 0; i < 9; i ++) brailletodigits[numberletters[i]] = String.fromCharCode(49+i);
+  }
+
+  if(allowspaces.checked){
+    brailletoletters[0] = ' '
+    brailletodigits[0] = ' '
+  }
+
+  if(allowsymbols.checked){
+    Object.assign(brailletoletters, {19:'.',16:',',24:';',18:':',12:'/',25:'?',26:'!',10:'#',11:'"',8:"'",49:'<',14:'>',27:'(',1:'^',9:'_',61:'&'})
+    Object.assign(brailletodigits, {19:'.',16:',',24:';',18:'-',12:'/',25:'?',26:'+',10:'#',11:'"',8:"'",49:'<',14:'>',27:'(',1:'^',9:'_',61:'&'})
+  }
+
+  decode();
+}
+
+function getchar(c){ // 'oo..o.' -> '110010'
   if(c == '?') return '?';
   if(c == '1' || c == 'o') return '1';
   if(c == '0' || c == '.') return '0';
+}
+
+function clean(l){
+  cleanl = "";
+  for(const c of l){
+    let x = getchar(c);
+    if(x) cleanl += x;
+  }
+  return cleanl
 }
 
 function matches(b,dots){
@@ -25,7 +62,7 @@ function matches(b,dots){
 }
 
 function getletter(dots){
-  if(dots == '??????') return '.';
+  if(dots == '??????') return '?';
   let possible = [];
   for(let b = 0; b < 64; b ++){
     if(matches(b.toString(2).padStart(6,'0'),dots)){
@@ -38,21 +75,29 @@ function getletter(dots){
   else return '['+possible.join('')+']';
 }
 
-function decodestring(input){
-  console.log(input);
-  let dots = [];
-  for(const c of input){
-    let x = getchar(c);
-    if(x) dots.push(x);
+function getdigit(dots){
+  if(dots == '??????') return '?';
+  let possible = [];
+  for(let b = 0; b < 64; b ++){
+    if(matches(b.toString(2).padStart(6,'0'),dots)){
+      let c = brailletodigits[b];
+      if(c) possible.push(c);
+    }
   }
-  while(dots % 6) dots.push('?');
+  possible.sort()
+  if(possible.length == 1) return possible[0];
+  else return '['+possible.join('')+']';
+}
+
+function decodestring(input){
+  while(input.length % 6) input += '?';
   ans = ""
   num = false  // whether 'numbers follow' or 'letters follow'
   openparen = false  // whether ( is found
   capital = false  // whether ^ is found
-  for(let i = 0; i < dots.length; i += 6){
-    if(!num) c = getletter(dots.slice(i,i+6).join(''));
-    else c = getdigit(dots.slice(i,i+6).join(''));
+  for(let i = 0; i < input.length; i += 6){
+    if(!num) c = getletter(input.slice(i,i+6));
+    else c = getdigit(input.slice(i,i+6));
     if(c == "#") num = !num;
     else if(c == "^") capital = true;
     else if(c == "("){
@@ -68,7 +113,9 @@ function decodestring(input){
 }
 
 function decode(){
-  inputs = inputcode.value.toLowerCase().split(/\r?\n/);
+  inputs_dirty = inputcode.value.toLowerCase().split(/\r?\n/);
+  inputs = []
+  for(const l of inputs_dirty) inputs.push(clean(l));
   if(inputs.length % 3 == 0){ // then its the 3x2 grid layout
     newinputs = [];
     for(let i = 0; i < inputs.length/3; i ++){
@@ -84,7 +131,19 @@ function decode(){
     inputs = newinputs;
   }
   ans = inputs.map(decodestring).join('\n');
+
+  if(nutrimatic.checked){
+    ans = ans.toLowerCase().replace(/\?/g,'A')
+  }
+
   output.innerText = ans;
   if(ans.includes('[]')) malformed.innerText = "Your input might be malformed";
   else malformed.innerText = "Decoded text:";
 }
+
+inputcode.addEventListener("input", () => { if(inputcode.value.length < 1000) decode(); });
+allowdigits.addEventListener("input", inittable);
+allowspaces.addEventListener("input", inittable);
+allowsymbols.addEventListener("input", inittable);
+nutrimatic.addEventListener("input", inittable);
+inittable();
--- apps/cipher/semaphore.js
+++ apps/cipher/semaphore.js
@@ -4,22 +4,44 @@ const inputcode = document.getElementById('inputcode')
 const output = document.getElementById('output')
 const malformed = document.getElementById('malformed')
 
+const allowdigits = document.getElementById('allowdigits')
+const allowspaces = document.getElementById('allowspaces')
+const nutrimatic = document.getElementById('nutrimatic')
+
 // actual code
 
-numberletters = [[1,2],[2,4],[2,7],[2,8],[2,9],[2,6],[2,3],[1,4],[1,7],[6,8],[1,8],[1,9],[1,6],[1,3],[4,7],[4,8],[4,9],[4,6],[3,4],[7,8],[7,9],[3,8],[6,9],[3,9],[6,7],[3,6]]
-flagtoletters = {22:' ',89:'#',98:'#'}
-flagtodigits = {22:' ',68:'#',86:'#',18:'0',81:'0'}
+let flagtoletters
+let flagtodigits
 
-for(let i = 0; i < 26; i ++){
-  [x,y] = numberletters[i];
-  flagtoletters[10*x+y] = String.fromCharCode(97+i);
-  flagtoletters[10*y+x] = String.fromCharCode(97+i);
-}
+function inittable(){
+  // initialize the conversion table
+  numberletters = [[1,2],[2,4],[2,7],[2,8],[2,9],[2,6],[2,3],[1,4],[1,7],[6,8],[1,8],[1,9],[1,6],[1,3],[4,7],[4,8],[4,9],[4,6],[3,4],[7,8],[7,9],[3,8],[6,9],[3,9],[6,7],[3,6]]
 
-for(let i = 0; i < 9; i ++){
-  [x,y] = numberletters[i];
-  flagtodigits[10*x+y] = String.fromCharCode(49+i);
-  flagtodigits[10*y+x] = String.fromCharCode(49+i);
+  flagtoletters = {}
+  flagtodigits = {}
+  for(let i = 0; i < 26; i ++){
+    [x,y] = numberletters[i];
+    flagtoletters[10*x+y] = String.fromCharCode(65+i);
+    flagtoletters[10*y+x] = String.fromCharCode(65+i);
+  }
+
+  if(allowdigits.checked){
+    flagtoletters[89] = '#'
+    flagtoletters[98] = '#'
+    flagtodigits = {68:'#',86:'#',18:'0',81:'0'}
+    for(let i = 0; i < 9; i ++){
+      [x,y] = numberletters[i];
+      flagtodigits[10*x+y] = String.fromCharCode(49+i);
+      flagtodigits[10*y+x] = String.fromCharCode(49+i);
+    }
+  }
+
+  if(allowspaces.checked){
+    flagtoletters[22] = ' '
+    flagtodigits[22] = ' '
+  }
+
+  decode();
 }
 
 function getchar(c){
@@ -29,9 +51,18 @@ function getchar(c){
   else if('zxcadqwe'.includes(c)) return ' zxca dqwe'.indexOf(c);
 }
 
+function clean(l){ // 'qwed' -> [7,8,9,6]
+  cleanl = []
+  for(const c of l){
+    let x = getchar(c)
+    if(x) cleanl.push(x)
+  }
+  return cleanl
+}
+
 function getletter(x,y){
   if(x!=5 && y==5) return getletter(y,x);
-  if(x==5 && y==5) return '.';
+  if(x==5 && y==5) return '?';
   possible = []
   if(x==5){
     for(let i = 1; i <= 9; i ++){
@@ -50,7 +81,7 @@ function getletter(x,y){
 
 function getdigit(x,y){
   if(x!=5 && y==5) return getletter(y,x);
-  if(x==5 && y==5) return '.';
+  if(x==5 && y==5) return '?';
   possible = []
   if(x==5){
     for(let i = 1; i <= 9; i ++){
@@ -68,17 +99,12 @@ function getdigit(x,y){
 }
 
 function decodestring(input){
-  let flags = []
-  for(const c of input){
-    let x = getchar(c);
-    if(x) flags.push(x);
-  }
-  if(flags.length % 2) flags.push(5);
+  if(input.length % 2) input.push(5);
   ans = ""
   num = false  // whether 'numbers follow' or 'letters follow'
-  for(let i = 0; i < flags.length; i += 2){
-    if(!num) c = getletter(flags[i],flags[i+1]);
-    else c = getdigit(flags[i],flags[i+1]);
+  for(let i = 0; i < input.length; i += 2){
+    if(!num) c = getletter(input[i],input[i+1]);
+    else c = getdigit(input[i],input[i+1]);
     if(c == "#") num = !num;
     else ans += c;
   }
@@ -86,8 +112,19 @@ function decodestring(input){
 }
 
 function decode(){
-  ans = inputcode.value.toLowerCase().split(/\r?\n/).map(decodestring).join('\n');
+  ans = inputcode.value.toLowerCase().split(/\r?\n/).map(clean).map(decodestring).join('\n');
+
+  if(nutrimatic.checked){
+    ans = ans.toLowerCase().replace(/\?/g,'A')
+  }
+
   output.innerText = ans;
   if(ans.includes('[]')) malformed.innerText = "Your input might be malformed";
   else malformed.innerText = "Decoded text:";
 }
+
+inputcode.addEventListener("input", () => { if(inputcode.value.length < 1000) decode(); });
+allowdigits.addEventListener("input", inittable);
+allowspaces.addEventListener("input", inittable);
+nutrimatic.addEventListener("input", inittable);
+inittable();
