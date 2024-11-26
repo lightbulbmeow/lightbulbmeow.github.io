c6c5a8b8793b3f388831de86b06239445ae1d2ca
lightbulbmeow <62456216+lightbulbmeow@users.noreply.github.com>
2024-11-26 12:45:36 +0800

    new puzzle + partial answer checking

--- puzzles/submit.js
+++ puzzles/submit.js
@@ -4,6 +4,7 @@ let verdict = document.getElementById("verdict")
 
 let akariyep = "../emojis/akariyep.webp"
 let akarinope = "../emojis/akarinope.webp"
+let chokocheer = "../emojis/chokocheer.webp"
 
 function clean(s){
     return s.toUpperCase().replace(/[^A-Z0-9]+/g,'');
@@ -12,9 +13,12 @@ function clean(s){
 submit.onclick = function(){
     let useranswer = answer.value;
     let correctanswer = atob(answer.dataset.base64Answer);
+    let partialanswer = atob(answer.dataset.base64PartialAnswer).split("|");
     if(clean(useranswer) == clean(correctanswer)){
         answer.value = correctanswer
         verdict.innerHTML = "<b><span style='color:lime'>CORRECT!!!</span> <img src='" + akariyep + "' height=24></b>"
+    }else if(partialanswer.map(clean).includes(clean(useranswer))){
+        verdict.innerHTML = "<b><span style='color:yellow'>Keep going! <img src='" + chokocheer + "' height=24><br>(That is not yet the correct answer)</span></b>"
     }else{
         verdict.innerHTML = "<b><span style='color:red'>WRONG!!!</span> <img src='" + akarinope + "' height=24></b>"
     }
