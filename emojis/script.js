7443ccdcaf8ac35a332fa96e639fb3f8387e03da
lightbulbmeow <62456216+lightbulbmeow@users.noreply.github.com>
2024-12-25 18:33:49 +0800

    Slant is NP-complete

--- emojis/script.js
+++ emojis/script.js
@@ -5,7 +5,7 @@ let scripts = document.getElementsByTagName('script');
 let path = scripts[scripts.length-1].src;
 let mydir = path.split('/').slice(0, -1).join('/')+'/';
 
-let webpemojis = /:(akarinope|akarismug|akariyep|bebelove|chokocheer|chokodestroy|hacker|heh|ikuwow|kanadecry|kanaderamen|koishibaka|matelove|menheranerdglasses|menherateehee|mikuangery|mikuexcited|nanaadmire|nanabday|nanableh|nanacry|nanakiss|nanasmirk|nanastare|nanawave|nanaxd|neko|niko|pomicry|pomihappy|pominervous|serikalong|tere|vodkahug|zoneadmire|zonelmao|zonepeek):/g
+let webpemojis = /:(akarinope|akarismug|akariyep|bebelove|chokocheer|chokodestroy|hacker|heh|ikuwow|kanadecry|kanaderamen|koishibaka|matelove|menheranerdglasses|menherateehee|mikuangery|mikuexcited|nanaadmire|nanaadmiresanta|nanabday|nanableh|nanacry|nanakiss|nanasmirk|nanastare|nanawave|nanaxd|neko|niko|pomicry|pomihappy|pominervous|serikalong|tere|vodkahug|zoneadmire|zonelmao|zonepeek):/g
 let gifemojis = /:(bocchihide|nanaaaa|nanaclap|nanayay):/g
 
 document.body.innerHTML = document.body.innerHTML.replace(webpemojis, '<img src="' + mydir + '$1.webp" height="24" />');
